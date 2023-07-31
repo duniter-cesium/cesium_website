79dc4163e187be3533ef346696a78b86aaf51119
Benoit Lavenier <benoit.lavenier@e-is.pro>
2023-07-31 19:11:39 +0200

    Release 1.7.5

--- cesiumVersions.php
+++ cesiumVersions.php
@@ -1,10 +1,10 @@
 <?php
 
 $cesiumVersions = [
-	   'home' => '1.7.3',
-	'android' => '1.7.3',
-	 'debian' => '1.6.12',
+	   'home' => '1.7.5',
+	'android' => '1.7.5',
+	 'debian' => '1.7.5',
  	   'arch' => '1.6.1', 
 	'windows' => '1.6.12',
-	    'web' => '1.7.3'
+	    'web' => '1.7.5'
 ];
